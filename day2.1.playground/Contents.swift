9ab3a00875a06a118ac7fd6afc1be21090cfb752
Amnah <amnaaali22@gmail.com>
2022-06-25 19:25:23 +0300

    day2.1

---
+++ day2.1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var name = "Amnah"
+var hobby = "Volleyball"
+var favouriteNumber = 22
+let BirthOfDate = 1999
+
+print("hi my name is \(name) \nmy favorite number \(favouriteNumber) \nI like \(hobby) \nand I'm born in \(BirthOfDate)")
+print("\n")
+print("سلام عليكم اسمي \(name) رمقمي المفضل هو \(favouriteNumber) هوايتي \(hobby) \n وانا من مواليد \(BirthOfDate) ")
